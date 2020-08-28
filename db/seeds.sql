8961c602c31498c96a331ae818e49046d981e593
Hema <khemalatha507@gmail.com>
2020-08-28 18:28:21 -0400

    created config folder connection.js/orm.js

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO burgers (burger_name)
+    values(`CheeseBurger`);
+
+INSERT INTO burgers (burger_name)
+    values(`Hamburger`);
+    
+INSERT INTO burgers (burger_name,devoured)
+    values(`Veggie Burger`);
+    
+INSERT INTO burgers (burger_name)
+    values(`Blackbean Burger`);
+    
+INSERT INTO burgers (burger_name)
+    values(`Chicken Burger`);
+    
+SELECT *
+FROM burgers;
+
+
+
